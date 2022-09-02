97259d7a3292c4a0818bbe5ffbd559d6642e5b8c
Andrew Sarver <ASarver@covermymeds.com>
2022-09-02 14:03:01 -0400

    list that toggles favorite flag on object

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const app = Vue.createApp({
   data() {
     return {
-      
+      pokemon: [
+        { name: 'Bulbasaur', type: 'Grass', img: 'assets/001Bulbasaur.webp', isFav: false },
+        { name: 'Charmander', type: 'Fire', img: 'assets/004Charmander.webp', isFav: true },
+        { name: 'Squirtle', type: 'Water', img: 'assets/007Squirtle.webp', isFav: true },
+      ]
     }
   },
   methods: {
-    
+    toggleFav(pkmn) {
+      pkmn.isFav = !pkmn.isFav
+    }
   }
 })
 
